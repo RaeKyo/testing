de5fce8026e70356d954ac4d706587795838e676
RaeKyo <tienhanv@gmail.com>
2021-06-09 08:33:26 +0700

    Update Folder inside to test

---
+++ Pattern C/Folder inside LOL/draw2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	int row,column;
+	printf ("Enter row :");
+	scanf("%d",&row);
+	printf ("\nEnter column :");
+	scanf("%d",&column);
+	
+	for (int i=0;i<=row;i++){
+		printf("\t");
+		for (int j=1;j<=column;j++){
+			printf("%d",i);
+		}
+		printf("\n");
+	}
+	printf("This is the box %d x %d ~",row , column);
+	printf("\nNoice :D");
+	getchar();
+	return 0;
+
+}
